c65f86a3f81f46badbd71a9eddc2352b67143dc1
Juan Lopez <jucalo2195@gmail.com>
2023-03-16 14:39:47 -0500

    Validaciones Formik y Yup

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useFormik, Formik, Field} from 'formik'
+import { useFormik} from 'formik'
+import * as Yup from 'yup'
 
 const Create = () => {
 
@@ -7,6 +8,22 @@ const Create = () => {
         {
             initialValues:{
                 titulo:'',descripcion:'',categoria:'',equipo:'',link:''},
+                validationSchema: Yup.object({
+                    titulo:Yup.string().required('Ingrese el titulo del deporte')
+                    .max(20,'Maximo 20 caracteres')
+                    .min(3,'Minimo 3 caracteres'),
+                    descripcion:Yup.string().required('Ingrese la descripcion del deporte')
+                    .max(200,'Maximo 200 caracteres')
+                    .min(50,'Minimo 50 caracteres'),
+                    categoria:Yup.string().required('Elija la categoria del deporte')
+                    .oneOf(["Equipo","Mesa","Motor"],"Selecciona una de las opciones"),
+                    equipo:Yup.string().required('Ingrese el equipo')
+                    .max(20,'Maximo 15 caracteres')
+                    .min(3,'Minimo 3 caracteres'),
+                    link:Yup.string().required('Ingrese el link a la pagina del equipo')
+                    .max(20,'Maximo 50 caracteres')
+                    .min(3,'Minimo 10 caracteres')
+                }),
                 onSubmit: values=>{
                 console.log(values)
             }
@@ -22,7 +39,13 @@ const Create = () => {
                 name='titulo'
                 id='titulo'
                 onChange={formik.handleChange}
+                value={formik.values.titulo}
+                onBlur={formik.handleBlur}
                 />
+                {
+                formik.touched.titulo && formik.errors.titulo ?
+                <div>{formik.errors.titulo}</div>:null
+                }
             </div>
             <div>
                 <label>Descripción:</label>
@@ -30,16 +53,29 @@ const Create = () => {
                 name='descripcion'
                 id='descripcion'
                 onChange={formik.handleChange}
+                value={formik.values.descripcion}
+                onBlur={formik.handleBlur}
                 />
+                {
+                formik.touched.descripcion && formik.errors.descripcion ?
+                <div>{formik.errors.descripcion}</div>:null
+                }
             </div>
             <div>
                 <label>Categoria:</label>
-                <select name="categoria" id="descripcion" onChange={formik.handleChange}>
+                <select name="categoria" id="descripcion" 
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                >
                     <option value="Todos">Seleccionar deporte</option>
                     <option value="Equipo">Deporte de equipo</option>
                     <option value="Mesa">Deporte de mesa</option>
                     <option value="Motor">Deporte de motor</option>
                 </select>
+                {
+                formik.touched.categoria && formik.errors.categoria ?
+                <div>{formik.errors.categoria}</div>:null
+                }
             </div>
             <div>
                 <label>Equipo:</label>
@@ -47,7 +83,13 @@ const Create = () => {
                 name='equipo'
                 id='equipo'
                 onChange={formik.handleChange}
+                value={formik.values.equipo}
+                onBlur={formik.handleBlur}
                 />
+                {
+                formik.touched.equipo && formik.errors.equipo ?
+                <div>{formik.errors.equipo}</div>:null
+                }
             </div>
             <div>
                 <label>Link:</label>
@@ -55,7 +97,13 @@ const Create = () => {
                 name='link'
                 id='link'
                 onChange={formik.handleChange}
+                value={formik.values.link}
+                onBlur={formik.handleBlur}
                 />
+                {
+                formik.touched.link && formik.errors.link ?
+                <div>{formik.errors.link}</div>:null
+                }
             </div>
             <input type="submit" value="Enviar"/>
         </form>
